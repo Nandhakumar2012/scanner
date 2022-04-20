65043f6f37dc1d2b6f5e41683fd7ac354123d832
Nandhakumar2012 <nandha2996@gmail.com>
2022-04-20 10:00:02 +0530

    nan

---
+++ src/org/test/Flipcarttest.java
@@ -0,0 +1,48 @@
+package org.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipcarttest
+{
+  public static void main(String[] args)
+  {
+	 
+	System.setProperty("webdriver.chrome.driver", "C:\\ProjectClassPractice\\driver\\chromedriver.exe");
+	
+   WebDriver driver=new ChromeDriver();
+   
+   driver.get("https://www.flipkart.com/");
+   
+   driver.manage().window().maximize();
+   
+   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+   
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+	 
+	 
+	 
+	 
+	 
+}
+}
